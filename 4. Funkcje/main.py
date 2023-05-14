902248a7d89410153de3255921c0b1869f6760c8
JakubRobo <131374444+JakubRobo@users.noreply.github.com>
2023-05-14 19:09:46 +0200

    funkcje, z pętlą

--- 4. Funkcje/main.py
+++ 4. Funkcje/main.py
@@ -1,21 +1,31 @@
-print('Kalkulator')
-print('1. dodawanie')
-print('2. odejmowanie')
-print('3. mnożenie')
-print('4. dzielenie')
+while True:
+    print('Kalkulator')
+    print('1. dodawanie')
+    print('2. odejmowanie')
+    print('3. mnożenie')
+    print('4. dzielenie')
+    print('5. wyjście')
+    dzialanie = input("Podaj działanie: ")
+    if dzialanie == '1':  
+      a = int(input('Podaj pierwszą liczbę: '))
+      b = int(input('Podaj drugą liczbę: '))
+      wynik = a + b
+    elif dzialanie == '2':
+      a = int(input('Podaj pierwszą liczbę: '))
+      b = int(input('Podaj drugą liczbę: '))
+      wynik = a - b
+    elif dzialanie == '3':
+      a = int(input('Podaj pierwszą liczbę: '))
+      b = int(input('Podaj drugą liczbę: '))
+      wynik = a * b
+    elif dzialanie == '4':
+      a = int(input('Podaj pierwszą liczbę: '))
+      b = int(input('Podaj drugą liczbę: '))
+      wynik = a / b
+    elif dzialanie == '5':
+        break  
+    else:
+        wynik = 'Nie ma takiego działania'
 
-a = int(input('Podaj pierwszą liczbę '))
-b = int(input('Podaj drugą liczbę '))
-
-dzialanie = input('Wybierz działanie ')
-if dzialanie == '1':
-      wynik = a+b
-elif dzialanie == '2':
-      wynik = a-b
-elif dzialanie == '3':
-      wynik = a*b
-elif dzialanie == '4':
-      wynik = a/b
-else:
-      wynik = 'nie ma takiego dzialania'
-print(wynik)
+    print("Wynik:", wynik)
+    print()  
