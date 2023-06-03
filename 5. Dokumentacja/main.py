3c2ec1621933346ebeb840fa9a4766b1af0e8ed9
JakubRobo <robaszewskijakub@gmail.com>
2023-06-03 22:50:19 +0200

    poprawki programu, dodanie doc stringów
    dodatkowo lekkie zmiany wewnątrz kodu

--- 5. Dokumentacja/main.py
+++ 5. Dokumentacja/main.py
@@ -1,55 +1,46 @@
-from typing import Union
-
 def kalkulator() -> None:
-    while True:
-        print('Kalkulator')
-        print('1. dodawanie')
-        print('2. odejmowanie')
-        print('3. mnożenie')
-        print('4. dzielenie')
-        print('5. wyjście')
+    """Wyświetl menu"""
+    print('Kalkulator')
+    print('1. dodawanie')
+    print('2. odejmowanie')
+    print('3. mnożenie')
+    print('4. dzielenie')
+    print('5. wyjście')
+def dodawanie(a: float, b: float) -> None:
+    """Wyświetl sumę"""
+    print(f"\nSuma wynosi: {a+b}")
 
-        dzialanie = input("Podaj działanie: ")
-        if dzialanie == '1':
-            a = int(input('Podaj pierwszą liczbę: '))
-            b = int(input('Podaj drugą liczbę: '))
-            wynik = dodawanie(a, b)
-        elif dzialanie == '2':
-            a = int(input('Podaj pierwszą liczbę: '))
-            b = int(input('Podaj drugą liczbę: '))
-            wynik = odejmowanie(a, b)
-        elif dzialanie == '3':
-            a = int(input('Podaj pierwszą liczbę: '))
-            b = int(input('Podaj drugą liczbę: '))
-            wynik = mnozenie(a, b)
-        elif dzialanie == '4':
-            a = int(input('Podaj pierwszą liczbę: '))
-            b = int(input('Podaj drugą liczbę: '))
-            wynik = dzielenie(a, b)
-        elif dzialanie == '5':
-            break  
-        else:
-            wynik = 'Nie ma takiego działania'
+def odejmowanie(a: float, b: float) -> None:
+    """Wyświetl różnicę"""
+    print(f"\nRóżnica wynosi: {a-b}")
 
-        print("Wynik:", wynik)
-        print() 
+def mnozenie(a: float, b: float) -> None:
+    """Wyświetl iloczyn"""
+    print(f"\nIloczyn wynosi: {a*b}")
 
-
-def dodawanie(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-    return a + b
-
-
-def odejmowanie(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-    return a - b
-
-def mnozenie(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-    return a * b
-
-def dzielenie(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+def dzielenie(a: float, b: float) -> None:
+    """Wyświetl iloraz"""
     if b != 0:
-        return a / b
+        print(f"\nDzielenie wynosi: {a/b}")      
     else:
-        return 'Dzielenie przez zero!'
+        print("\nDzielenie przez 0!!!")
 
 
-kalkulator()
+while True:
+    kalkulator()
+    dzialanie = int(input("Podaj działanie: "))
+    if dzialanie == '1':
+        dodawanie(int(input("Podaj liczbę a: ")), int(input("Podaj liczbę b: ")))
+    elif dzialanie == '2':
+        odejmowanie(int(input("Podaj liczbę a: ")), int(input("Podaj liczbę b: ")))
+    elif dzialanie == '3':
+        mnozenie(int(input("Podaj liczbę a: ")), int(input("Podaj liczbę b: ")))
+    elif dzialanie == '4':
+        dzielenie(int(input("Podaj liczbę a: ")), int(input("Podaj liczbę b: ")))
+    elif dzialanie == '5':
+        break 
+
+
+
+
+
